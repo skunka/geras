4120ee400dd89a9340bc62a5204339c0c596636a
skunka <andrius@andajus.com>
2011-05-14 14:16:10 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geras::Application.config.secret_token = 'ec67376f7b97957b1a58ba50b33943cc2c5847580f24832e346d3a15e6372060db4e5e735a3e29121b6edf0fe1e1c99da8dfb45a94fd1b2501d5ca0da3b127cc'
